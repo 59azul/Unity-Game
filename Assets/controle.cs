6ab2f374d3872da788cdeb36252e0ef621e85af3
59azul <danilopaixao00@gmail.com>
2016-04-02 07:31:00 -0300

    Movimento básico

--- Assets/controle.cs
+++ Assets/controle.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class controle : MonoBehaviour {
 
+    public float movespeed, jumpSpeed;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,20 @@ public class controle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movespeed, GetComponent<Rigidbody2D>().velocity.y); // seta a velocidade do corpo rigido  para movespeed em x and mantem a velocidade em y
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movespeed, GetComponent<Rigidbody2D>().velocity.y); // seta a velocidade do corpo rigido  para movespeed em x and mantem a velocidade em y
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
+        }
+
+
+
+    }
 }
